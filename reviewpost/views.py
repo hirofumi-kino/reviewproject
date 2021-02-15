d7db2c1ffcc66ade49aef21beffd632bed59167c
hirofumi-kino <kjhnft@gmail.com>
2021-02-16 00:18:04 +0900

    signup機能、try-except実装

--- reviewpost/views.py
+++ reviewpost/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 
 
 def signupview(request):
     if request.method == 'POST':
-        print('POST method')
+        username_data = request.POST['username_data']
+        password_data = request.POST['password_data']
+        try:
+            user = User.objects.create_user(username_data, '', password_data)
+        except IntegrityError:
+            return render(request, 'signup.html', {'error': 'このユーザーはすでに登録されています'})
     else:
-        print('GET method probably・・・')
+        print(User.objects.all())
     return render(request, 'signup.html', {})
