68c8a1dd5a95bd0e0ac89c496b9225b7eb2da673
hirofumi-kino <kjhnft@gmail.com>
2021-02-18 00:36:29 +0900

    {}→[]に修正

--- reviewpost/urls.py
+++ reviewpost/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import signupview
+from .views import signupview, loginview
 
-urlpatterns = {
+urlpatterns = [
     path('signup/', signupview, name='signup'),
-}
+    path('login/', loginview, name='login'),
+]
